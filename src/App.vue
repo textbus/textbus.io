963174387e329c29a165f1e583d73fac7ec725aa
tanbo <464328895@qq.com>
2022-02-16 23:07:23 +0800

    feat: 迁移通用 css

--- src/App.vue
+++ src/App.vue
@@ -1,30 +1,25 @@
 <template>
-  <div id="nav">
-    <router-link to="/">Home</router-link> |
-    <router-link to="/about">About</router-link>
-  </div>
+  <header>
+    <div class="ui-container">
+      <div class="ui-row">
+        <div class="ui-pull-left">
+          <div>
+            <router-link to="/">Home</router-link> |
+            <router-link to="/about">About</router-link>
+          </div>
+          <div class="ui-pull-right">
+
+          </div>
+        </div>
+      </div>
+    </div>
+  </header>
   <router-view/>
 </template>
 
-<style lang="scss">
-#app {
-  font-family: Avenir, Helvetica, Arial, sans-serif;
-  -webkit-font-smoothing: antialiased;
-  -moz-osx-font-smoothing: grayscale;
-  text-align: center;
-  color: #2c3e50;
-}
-
-#nav {
-  padding: 30px;
-
-  a {
-    font-weight: bold;
-    color: #2c3e50;
-
-    &.router-link-exact-active {
-      color: #42b983;
-    }
-  }
+<style lang="scss" scoped>
+nav {
+  height: 50px;
+  padding: 10px 0;
 }
 </style>
