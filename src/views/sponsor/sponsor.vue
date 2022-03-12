f52d8706ff18b86f535b5ec9fbead8108e6a4077
tanbo <464328895@qq.com>
2022-03-12 23:03:32 +0800

    feat：编写文档

--- src/views/sponsor/sponsor.vue
+++ src/views/sponsor/sponsor.vue
@@ -6,11 +6,11 @@
         <h2>合作与赞助</h2>
         <p>Textbus 的成长离不开社会的支持，如果 Textbus 为你带来了帮助，并且你也认同为知识付费，同时鼓励我们做的更好，欢迎通过下面的二维码表达你的支持</p>
         <div>
-          <img src="https://textbus.tanboui.com/alipay.jpg" style="width:230px;height:auto">&nbsp;&nbsp;&nbsp;&nbsp;<img
-            src="https://textbus.tanboui.com/wx.jpg" style="width:230px;height:auto"></div>
+          <img src="../../assets/alipay.png" style="width:230px;height:auto">&nbsp;&nbsp;&nbsp;&nbsp;<img
+            src="../../assets/wxpay.png" style="width:230px;height:auto"></div>
         <p>如果你有特殊的需求或想要和我们一起开发 Textbus 的后续功能，可以先加入 Textbus 的官方交流 QQ 群（786923770），然后和群主联系。</p>
         <div>
-          <img src="https://textbus.tanboui.com/qq-group.jpg" style="width:252px;height:auto"></div>
+          <img src="../../assets/qq-group.png" style="width:252px;height:auto"></div>
       </div>
       <div class="ui-col-md-4"></div>
     </div>
