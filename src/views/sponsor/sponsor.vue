62211adbdbd18c7f674c209a3ae06d7ed2c4f1a2
tanbo <464328895@qq.com>
2022-03-12 23:01:31 +0800

    feat：编写文档

--- src/views/sponsor/sponsor.vue
+++ src/views/sponsor/sponsor.vue
@@ -3,18 +3,13 @@
     <div class="ui-row">
       <div class="ui-col-md-4"></div>
       <div class="ui-col-md-16">
-        <h1 style="font-size:24px">
-          合作与赞助</h1>
-        <p style="font-size:15px;letter-spacing:0.5px;line-height:1.68em">
-          Textbus 的成长离不开社会的支持，如果 Textbus 为你带来了帮助，并且你也认同为知识付费，同时鼓励我们做的更好，欢迎通过下面的二维码表达你的支持</p>
-        <div
-            style="text-align:center;font-size:15px">
+        <h2>合作与赞助</h2>
+        <p>Textbus 的成长离不开社会的支持，如果 Textbus 为你带来了帮助，并且你也认同为知识付费，同时鼓励我们做的更好，欢迎通过下面的二维码表达你的支持</p>
+        <div>
           <img src="https://textbus.tanboui.com/alipay.jpg" style="width:230px;height:auto">&nbsp;&nbsp;&nbsp;&nbsp;<img
             src="https://textbus.tanboui.com/wx.jpg" style="width:230px;height:auto"></div>
-        <p style="font-size:15px;letter-spacing:0.5px;line-height:1.68em">
-          如果你有特殊的需求或想要和我们一起开发 Textbus 的后续功能，可以先加入 Textbus 的官方交流 QQ 群（786923770），然后和群主联系。</p>
-        <div
-            style="text-align:center;font-size:15px">
+        <p>如果你有特殊的需求或想要和我们一起开发 Textbus 的后续功能，可以先加入 Textbus 的官方交流 QQ 群（786923770），然后和群主联系。</p>
+        <div>
           <img src="https://textbus.tanboui.com/qq-group.jpg" style="width:252px;height:auto"></div>
       </div>
       <div class="ui-col-md-4"></div>
@@ -23,7 +18,12 @@
 </template>
 <style scoped lang="scss">
 .ui-container {
-  padding-top: 30px;
   padding-bottom: 30px;
+  color: #555;
+  line-height: 1.5;
+
+  h2, h3, h4 {
+    margin-top: 2em;
+  }
 }
 </style>
