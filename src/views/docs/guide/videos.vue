7d7c03d677698b39343284f18019c6a886565ef7
tanbo <464328895@qq.com>
2023-03-24 21:44:39 +0800

    feat：编写文档

--- src/views/docs/guide/videos.vue
+++ src/views/docs/guide/videos.vue
@@ -9,6 +9,12 @@ useDocUpdate(doc)
   <div ref="doc">
     <h1>视频教程</h1>
     <p>以下是 Textbus 官方群的在线分享视频，你可以通过视频更直观的了解 Textbus。</p>
+    <h2>从零开始创建一个协作富文本编辑器</h2>
+    <p>2023-03-24</p>
+    <p>本视频分享了使用 Textbus 内核和浏览器平台层创建一个完全自定义的富文本编辑器的入门知识。</p>
+    <div>
+      <iframe src="//player.bilibili.com/player.html?aid=611612276&bvid=BV1B84y1u7SA&cid=1069234432&page=1" scrolling="no" border="0" frameborder="no" framespacing="0" allowfullscreen="true"> </iframe>
+    </div>
     <h2>TodoList 组件开发实战</h2>
     <p>2022-05-07</p>
     <p>本视频分享了使用 Textbus 开发一个 TodoList 组件的要点及注意事项。</p>
