5e8ba2e8dc29c8525f8ed4b70153fccd37a78f6d
tbhuabi <464328895@qq.com>
2023-04-14 20:51:28 +0800

    feat：编写文档

--- src/views/docs/guide/videos.vue
+++ src/views/docs/guide/videos.vue
@@ -9,6 +9,12 @@ useDocUpdate(doc)
   <div ref="doc">
     <h1>视频教程</h1>
     <p>以下是 Textbus 官方群的在线分享视频，你可以通过视频更直观的了解 Textbus。</p>
+    <h2>使用 Vue/React 开发 Textbus 扩展</h2>
+    <p>2023-04-14</p>
+    <p>本视频分享了在 Vue 或 React 中，怎么简便的开发 Textbus 的工具或 UI。</p>
+    <div>
+      <iframe src="//player.bilibili.com/player.html?aid=952468399&bvid=BV1vs4y1P7DE&cid=1095656000&page=1" scrolling="no" border="0" frameborder="no" framespacing="0" allowfullscreen="true"> </iframe>
+    </div>
     <h2>从零开始创建一个协作富文本编辑器</h2>
     <p>2023-03-24</p>
     <p>本视频分享了使用 Textbus 内核和浏览器平台层创建一个完全自定义的富文本编辑器的入门知识。</p>
