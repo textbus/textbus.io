c3cf750bd4934ae87ed3395444db178ebedb8737
tbhuabi <464328895@qq.com>
2023-09-04 23:25:50 +0800

    feat：编写文档

--- src/pages/guide/guide.tsx
+++ src/pages/guide/guide.tsx
@@ -31,6 +31,12 @@ export function Guide() {
               <li>
                 <Link to="./slot" active="active" exact>插槽与格式</Link>
               </li>
+              <li>
+                <Link to="./slot" active="active" exact>插槽管理</Link>
+              </li>
+              <li>
+                <Link to="./member" active="active" exact>属性和方法</Link>
+              </li>
             </ul>
             <h3>进阶</h3>
             <ul>
@@ -39,6 +45,36 @@ export function Guide() {
               </li>
               <li>
                 <Link to="./hooks" active="active" exact>Hooks 和生命周期</Link>
+              </li>
+              <li>
+                <Link to="./collab" active="active" exact>文档解析与兼容处理</Link>
+              </li>
+              <li>
+                <Link to="./collab" active="active" exact>历史记录</Link>
+              </li>
+              <li>
+                <Link to="./collab" active="active" exact>状态查询</Link>
+              </li>
+              <li>
+                <Link to="./collab" active="active" exact>协作编辑</Link>
+              </li>
+            </ul>
+            <h3>高级</h3>
+            <ul>
+              <li>
+                <Link to="./hooks" active="active" exact>插件和模块</Link>
+              </li>
+              <li>
+                <Link to="./hooks" active="active" exact>单元测试</Link>
+              </li>
+              <li>
+                <Link to="./hooks" active="active" exact>跨平台基础</Link>
+              </li>
+            </ul>
+            <h3>其它</h3>
+            <ul>
+              <li>
+                <Link to="./hooks" active="active" exact>Vue 和 React 应用</Link>
               </li>
             </ul>
           </nav>
